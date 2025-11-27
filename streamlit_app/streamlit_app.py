77c5973e0062edbfa6a9184bd5d7fe0be0e3cb26
manisudhan <manisudhan19@gmail.com>
2025-11-28 02:04:55 +0530

    updated streamlit app

--- streamlit_app/streamlit_app.py
+++ streamlit_app/streamlit_app.py
@@ -309,11 +309,7 @@ def display_kpis(df_all_loc_filtered, df_type_filtered):
 
 def main():
 
-    st.write("Secrets loaded:", list(st.secrets.keys()))
-    st.write("MAPBOX exists:", "MAPBOX_API_KEY" in st.secrets)
-    st.write("MAPBOX value starts with pk:", st.secrets.get("MAPBOX_API_KEY", "").startswith("pk"))
-    import pydeck as pdk
-    st.write("PyDeck version:", pdk.__version__)
+    
 
     st.title("🚨 Traffic Violation Analysis Dashboard")
     st.markdown("A visual analytics interface powered by **pre-aggregated PySpark outputs**.")
